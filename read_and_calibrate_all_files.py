e94973be77925b599f1378a504411e11edc2e9f3
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2021-05-06 09:37:10 +0200

    made into main function callable from terminal

--- read_and_calibrate_all_files.py
+++ read_and_calibrate_all_files.py
@@ -17,38 +17,63 @@ import matplotlib.pyplot as plt
 from L1_calibrate import L1_calibrate
 from LindasCalibrationFunctions import plot_CCDimage
 
-directory = "data/"
-read_from = "rac"
-CCDitems = read_all_files_in_directory(read_from, directory)
+import argparse
 
 
-calibrate = True
-plot = True
+def main(directory="data/"):
 
-if calibrate:
-    for CCDitem in CCDitems[:3]:
-        (
-            image_lsb,
-            image_bias_sub,
-            image_desmeared,
-            image_dark_sub,
-            image_flatf_comp,
-        ) = L1_calibrate(CCDitem)
+    read_from = "rac"
+    CCDitems = read_all_files_in_directory(read_from, directory)
 
-        if plot == True:
-            fig, ax = plt.subplots(5, 1)
-            plot_CCDimage(image_lsb, fig, ax[0], "Original LSB")
-            plot_CCDimage(image_bias_sub, fig, ax[1], "Bias subtracted")
-            plot_CCDimage(image_desmeared, fig, ax[2], " Desmeared LSB")
-            plot_CCDimage(image_dark_sub, fig, ax[3], " Dark current subtracted LSB")
-            plot_CCDimage(image_flatf_comp, fig, ax[4], " Flat field compensated LSB")
-            fig.suptitle(CCDitem["channel"])
+    calibrate = True
+    plot = True
 
-else:
+    if calibrate:
+        for CCDitem in CCDitems[:3]:
+            (
+                image_lsb,
+                image_bias_sub,
+                image_desmeared,
+                image_dark_sub,
+                image_flatf_comp,
+            ) = L1_calibrate(CCDitem)
 
-    for CCDitem in CCDitems[:]:
-        fig = plt.figure()
-        ax = fig.gca()
-        plotCCDitem(CCDitem, fig, ax, title=CCDitem["channel"])
+            if plot == True:
+                fig, ax = plt.subplots(5, 1)
+                plot_CCDimage(image_lsb, fig, ax[0], "Original LSB")
+                plot_CCDimage(image_bias_sub, fig, ax[1], "Bias subtracted")
+                plot_CCDimage(image_desmeared, fig, ax[2], " Desmeared LSB")
+                plot_CCDimage(
+                    image_dark_sub, fig, ax[3], " Dark current subtracted LSB"
+                )
+                plot_CCDimage(
+                    image_flatf_comp, fig, ax[4], " Flat field compensated LSB"
+                )
+                fig.suptitle(CCDitem["channel"])
+
+    else:
+
+        for CCDitem in CCDitems[:]:
+            fig = plt.figure()
+            ax = fig.gca()
+            plotCCDitem(CCDitem, fig, ax, title=CCDitem["channel"])
+
+
+# %%
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Calibrates MATS images")
+    parser.add_argument(
+        "-i",
+        "-in_directory",
+        nargs="?",
+        default="data/",
+        dest="in_directory",
+        help="input diretory of mats images and metadata",
+    )
+
+    args = parser.parse_args()
+
+    main(args.in_directory)
 
 # %%
