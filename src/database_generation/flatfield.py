98bcfa450586c21e5f2ebe4e6f6d43f49cfe40d0
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2021-08-27 10:11:27 +0200

    added second package for database generation

---
+++ src/database_generation/flatfield.py
@@ -0,0 +1,5 @@
+import mats_l1_processing
+
+
+def make_flatfield():
+    print("this is a dummy function")
---
+++ tests/test_database_generation.py
@@ -0,0 +1,11 @@
+import pytest
+
+from database_generation.flatfield import make_flatfield
+
+__author__ = "Ole Martin Christensen"
+__copyright__ = "Ole Martin Christensen"
+__license__ = "MIT"
+
+
+def test_generate_flatfield():
+    make_flatfield()
