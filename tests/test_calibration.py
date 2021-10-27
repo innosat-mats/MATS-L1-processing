d849cd68951ad1e07cc7bc654bfef898070c95c0
lindamegner <linda@misu.su.se>
2021-10-27 15:55:32 +0200

    *NEW DATA* Add directory 210215OHBLimbImage to your tesdata folder. Added test_readfunctions to test_calibration.py

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -27,3 +27,24 @@ def test_calibrate_and_plot():
         calibrate=True,
         plot=True,
     )
+
+def test_readfunctions():
+    from mats_l1_processing.read_in_functions import readprotocol, read_all_files_in_root_directory
+    from mats_l1_processing.LindasCalibrationFunctions import read_all_files_in_protocol
+
+
+    directory='testdata/210215OHBLimbImage/'
+    protocol='protocol_dark_bright_100um_incl_IR3.txt'
+
+
+    read_from="rac" 
+    df_protocol=readprotocol(directory+protocol)
+
+    df_bright=df_protocol[df_protocol.DarkBright=='B']
+    CCDitems=read_all_files_in_protocol(df_bright, read_from,directory)
+
+    CCDitems=read_all_files_in_root_directory(read_from,directory)
+    
+    read_from="imgview" 
+    CCDitems=read_all_files_in_root_directory(read_from,directory)
+    
