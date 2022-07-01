936e7ddf534f8c2751f12187c21b51eecf05e449
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2022-07-01 09:55:34 +0200

    removed non linearity tests

--- tests/test_database_generation.py
+++ tests/test_database_generation.py
@@ -21,6 +21,6 @@ def test_generate_flatfield():
 def test_get_linearity():
     make_linearity([1], calibration_file, plot=False)
 
-def test_get_threshold():
-    p=np.array([0.98,-0.0000047, 6916])
-    assert np.abs((get_non_lin_important(p,0.8, fittype='threshold2')-30597.91319148936)) < 0.1 
+#def test_get_threshold():
+#    p=np.array([0.98,-0.0000047, 6916])
+#    assert np.abs((get_non_lin_important(p,0.8, fittype='threshold2')-30597.91319148936)) < 0.1 
