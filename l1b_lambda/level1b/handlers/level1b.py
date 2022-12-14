35c6ecb79d7d261167030f55076bcf6942da4282
Andreas Skyman <andreas.skyman@molflow.com>
2022-12-14 17:06:28 +0100

    Add better logging in Lambda

--- l1b_lambda/level1b/handlers/level1b.py
+++ l1b_lambda/level1b/handlers/level1b.py
@@ -29,6 +29,10 @@ class InvalidMessage(Exception):
     pass
 
 
+class Level1BException(Exception):
+    pass
+
+
 def get_env_or_raise(variable_name: str) -> str:
     if (var := os.environ.get(variable_name)) is None:
         raise EnvironmentError(
@@ -94,87 +98,98 @@ def get_instrument(
 
 
 def lambda_handler(event: Event, context: Context):
-    output_bucket = get_env_or_raise("L1B_BUCKET")
-    instrument_bucket = get_env_or_raise("INSTRUMENT_BUCKET")
-    region = os.environ.get('AWS_REGION', "eu-north-1")
-    s3 = pa.fs.S3FileSystem(region=region)
+    try:
+        output_bucket = get_env_or_raise("L1B_BUCKET")
+        instrument_bucket = get_env_or_raise("INSTRUMENT_BUCKET")
+        region = os.environ.get('AWS_REGION', "eu-north-1")
+        s3 = pa.fs.S3FileSystem(region=region)
+
+        try:
+            bucket, object = parse_event_message(event)
+            object_path = f"{bucket}/{object}"
+        except InvalidMessage:
+            return {
+                'statusCode': HTTPStatus.NO_CONTENT,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps({
+                    'message': 'Failed to parse event, nothing to do.'
+                })
+            }
+
+        if not object.endswith(".parquet"):
+            return {
+                'statusCode': HTTPStatus.NO_CONTENT,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps({
+                    'message': f'{object} is not a parquet file, nothing to do.'
+                })
+            }
+    except Exception as err:
+        raise Level1BException("Failed to initialize handler") from err
 
     try:
-        bucket, object = parse_event_message(event)
-        object_path = f"{bucket}/{object}"
-    except InvalidMessage:
-        return {
-            'statusCode': HTTPStatus.NO_CONTENT,
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps({
-                'message': 'Failed to parse event, nothing to do.'
-            })
-        }
+        with TemporaryDirectory(
+            "_instrument",
+            "/tmp",
+        ) as instrument_path:
+            rclone_config_path = get_rclone_config_path(
+                boto3.client("ssm"),
+                get_env_or_raise("RCLONE_CONFIG_SSM_NAME")
+            )
+            instrument = get_instrument(
+                instrument_path,
+                instrument_bucket,
+                rclone_config_path,
+            )
 
-    if not object.endswith(".parquet"):
-        return {
-            'statusCode': HTTPStatus.NO_CONTENT,
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps({
-                'message': f'{object} is not a parquet file, nothing to do.'
-            })
-        }
+            ccd_data = rpf.read_ccd_data(object_path, filesystem=s3)
+            ccd_items = rpf.dataframe_to_ccd_items(
+                ccd_data,
+                remove_empty=False,
+                remove_errors=False,
+                remove_warnings=False,
+            )
 
-    with TemporaryDirectory(
-        "_instrument",
-        "/tmp",
-    ) as instrument_path:
-        rclone_config_path = get_rclone_config_path(
-            boto3.client("ssm"),
-            get_env_or_raise("RCLONE_CONFIG_SSM_NAME")
+            for ccd in ccd_items:
+                if ccd["IMAGE"] is None:
+                    image_calibrated = None
+                    errors = None
+                else:
+                    (
+                        _,
+                        _,
+                        _,
+                        _,
+                        _,
+                        image_calibrated,
+                        _,
+                        errors,
+                    ) = L1_calibrate(ccd, instrument)
+                ccd["ImageCalibrated"] = image_calibrated
+                ccd["CalibrationErrors"] = errors
+    except Exception as err:
+        msg = f"Failed to process {object_path}"
+        raise Level1BException(msg) from err
+
+    try:
+        calibrated = DataFrame.from_records(
+            ccd_items,
+            columns=["EXP Date", "ImageCalibrated", "CalibrationErrors"],
         )
-        instrument = get_instrument(
-            instrument_path,
-            instrument_bucket,
-            rclone_config_path,
-        )
-
-        ccd_data = rpf.read_ccd_data(object_path, filesystem=s3)
-        ccd_items = rpf.dataframe_to_ccd_items(
+        l1b_data = concat([
             ccd_data,
-            remove_empty=False,
-            remove_errors=False,
-            remove_warnings=False,
+            calibrated,
+        ], axis=1).set_index("EXPDate").sort_index()
+        l1b_data.drop(["ImageData", "Errors", "Warnings"], axis=1, inplace=True)
+        l1b_data = l1b_data[l1b_data.ImageCalibrated != None]  # noqa: E711
+
+        out_table = pa.Table.from_pandas(l1b_data)
+        pq.write_table(
+            out_table,
+            f"{output_bucket}/{object_path}",
+            filesystem=s3,
+            version='2.6',
         )
-
-        for ccd in ccd_items:
-            if ccd["IMAGE"] is None:
-                image_calibrated = None
-                errors = None
-            else:
-                (
-                    _,
-                    _,
-                    _,
-                    _,
-                    _,
-                    image_calibrated,
-                    _,
-                    errors,
-                ) = L1_calibrate(ccd, instrument)
-            ccd["ImageCalibrated"] = image_calibrated
-            ccd["CalibrationErrors"] = errors
-
-    calibrated = DataFrame.from_records(
-        ccd_items,
-        columns=["EXP Date", "ImageCalibrated", "CalibrationErrors"],
-    )
-    l1b_data = concat([
-        ccd_data,
-        calibrated,
-    ], axis=1).set_index("EXPDate").sort_index()
-    l1b_data.drop(["ImageData", "Errors", "Warnings"], axis=1, inplace=True)
-    l1b_data = l1b_data[l1b_data.ImageCalibrated != None]  # noqa: E711
-
-    out_table = pa.Table.from_pandas(l1b_data)
-    pq.write_table(
-        out_table,
-        f"{output_bucket}/{object_path}",
-        filesystem=s3,
-        version='2.6',
-    )
+    except Exception as err:
+        msg = f"Failed to store {object_path}"
+        raise Level1BException(msg) from err
