d05b39849a3d8fe1ed9164bcd529588f5153b9cf
Andreas Skyman <skymandr@fripost.org>
2023-02-14 15:19:57 +0100

    Propagate version (#112)
    * Add gitpython requirement
    
    * Propagate code version to stack
    
    * Make it possible to return metadata for Parquet file
    
    * Save code version metadata in L1B parquet file
    
    * Add file metadata option to other ccd reader function too

--- src/mats_l1_processing/read_parquet_functions.py
+++ src/mats_l1_processing/read_parquet_functions.py
@@ -11,7 +11,7 @@ Parquet files can either be local or on a remote server, such as Amazon S3.
 import logging
 from datetime import datetime, timezone
 from io import BytesIO
-from typing import cast, Any, Dict, List, Optional, SupportsFloat
+from typing import cast, Any, Dict, List, Optional, SupportsFloat, Tuple, Union
 
 import numpy as np
 import pyarrow as pa  # type: ignore
@@ -185,9 +185,10 @@ def read_ccd_data_in_interval(
     stop: datetime,
     path: str,
     filesystem: Optional[pa.fs.FileSystem] = None,
-) -> DataFrame:
+    metadata: bool = False,
+) -> Union[DataFrame, Tuple[DataFrame, pq.FileMetaData]]:
     """Reads the CCD data and metadata from the specified path or S3 bucket
-    between the specified times.
+    between the specified times. Optionally read file metadata.
 
     Args:
         start (datetime):           Read CCD data from this time (inclusive).
@@ -197,9 +198,12 @@ def read_ccd_data_in_interval(
         filesystem (FileSystem):    Optional. File system to read. If not
                                     specified will assume that path points to
                                     an ordinary directory disk. (Default: None)
+        metadata (bool):            If True, return Parquet file metadata along
+                                    with data frame. (Default: False)
 
     Returns:
-        DataFrame:  The CCD data.
+        DataFrame:      The CCD data.
+        FileMetaData:   File metadata (optional).
     """
 
     if start.tzinfo is None:
@@ -207,13 +211,17 @@ def read_ccd_data_in_interval(
     if stop.tzinfo is None:
         stop.replace(tzinfo=timezone.utc)
 
-    return ds.dataset(
+    table = ds.dataset(
         path,
         filesystem=filesystem,
     ).to_table(filter=(
         (ds.field("EXPDate") >= Timestamp(start))
         & (ds.field("EXPDate") <= Timestamp(stop))
-    )).to_pandas().reset_index()
+    ))
+    dataframe = table.to_pandas().reset_index()
+    if metadata:
+        return dataframe, table.schema.metadata
+    return dataframe
 
 
 def read_ccd_items_in_interval(
@@ -248,8 +256,10 @@ def read_ccd_items_in_interval(
 def read_ccd_data(
     path: str,
     filesystem: Optional[pa.fs.FileSystem] = None,
-) -> DataFrame:
+    metadata: bool = False,
+) -> Union[DataFrame, Tuple[DataFrame, pq.FileMetaData]]:
     """Reads the CCD data and metadata from a singel file at the specified path.
+    Optionally read file metadata.
 
     Args:
         path (str):                 Path to dataset. May be a directory or a
@@ -257,15 +267,22 @@ def read_ccd_data(
         filesystem (FileSystem):    Optional. File system to read. If not
                                     specified will assume that path points to
                                     an ordinary directory disk. (Default: None)
+        metadata (bool):            If True, return Parquet file metadata along
+                                    with data frame. (Default: False)
 
     Returns:
-        DataFrame:  The CCD data.
+        DataFrame:      The CCD data.
+        FileMetaData:   File metadata (optional).
     """
 
-    return pq.read_table(
+    table = pq.read_table(
         path,
         filesystem=filesystem,
-    ).to_pandas().reset_index()
+    )
+    dataframe = table.to_pandas().reset_index()
+    if metadata:
+        return dataframe, table.schema.metadata
+    return dataframe
 
 
 def read_ccd_items(
