f960a4f3201adc10812cf15216f167ed7995ca01
Andreas Skyman <andreas.skyman@molflow.com>
2023-05-30 11:22:04 +0200

    Should actually be warn

--- src/mats_l1_processing/L1_calibration_functions.py
+++ src/mats_l1_processing/L1_calibration_functions.py
@@ -973,13 +973,13 @@ def artifact_correction(ccditem,image=None):
         len(artifact_masks) == 1
         and np.sum(np.abs(artifact_masks.iloc[0]['bias_mask']) == 0.0)
     ):
-        warnings.warning("Empty mask applied (no correction applied)")
+        warnings.warn("Empty mask applied (no correction applied)")
         return image, error_flag
 
     try:
         azimuth = ccditem["nadir_az"] # nadir azimuth angle of the ccditem
     except KeyError: # if not available, no correction is applied
-        warnings.warning(
+        warnings.warn(
             "Nadir solar azimuth angle unavailable (no correction applied)"
         )
         return image, error_flag
@@ -993,7 +993,7 @@ def artifact_correction(ccditem,image=None):
     mask = artifact_masks['bias_mask'][best_ind]
 
     if np.shape(mask) != np.shape(image):
-        warnings.warning(
+        warnings.warn(
             "Image shape doesn't match the mask shape (no correction applied)"
         )
         return image, error_flag
