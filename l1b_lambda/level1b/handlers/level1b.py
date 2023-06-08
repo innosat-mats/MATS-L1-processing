ae6c7302cdae146c554c5654040cb11bd5e0efc2
Andreas Skyman <andreas.skyman@molflow.com>
2023-06-08 11:44:31 +0200

    Fix so that bytes are not used for metadata columns, causing duplicates

--- l1b_lambda/level1b/handlers/level1b.py
+++ l1b_lambda/level1b/handlers/level1b.py
@@ -156,8 +156,8 @@ def lambda_handler(event: Event, context: Context):
         raise Level1BException(msg) from err
 
     try:
-        for key in metadata.keys():
-            l1b_data[key] = metadata[key]
+        for key, val in metadata.items():
+            l1b_data[key.encode()] = val.encode()
         out_table = pa.Table.from_pandas(l1b_data)
         out_table = out_table.replace_schema_metadata({
             **metadata,
