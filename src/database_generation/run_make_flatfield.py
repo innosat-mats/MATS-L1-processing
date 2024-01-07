9471c43a872b2b2118e234cd9ca27c0e01a4b12f
lindamegner <linda@misu.su.se>
2024-01-07 09:22:05 +0100

    1: Changed so that the 3 flatfields with different integration time that are used to generate the non-baffle flatfield are scaled to be unity in an area at the center of the image. This was not done before and gave errors larger than they should be. An option to scale the std error to the mean of the signal was added to the function scale_field. Scalefield then reports
    2: run_make_flatfield.py now also calculates and saves the error of the flatfield, calculated as the standard
    deviation of the 3 flatfields used to construct the flatfield, divided by sqrt(3).
    
    3: run_make_flatfield also now saves the baffle_scalefield,
    which is the scaling factor used to
    multiply the non-baffle flatfield with to get the merged flatfield.
    This is unity in the main part of the image where there is no baffle interference.
    
    4: Added an optional plot to the make_flatfield function

--- src/database_generation/run_make_flatfield.py
+++ src/database_generation/run_make_flatfield.py
@@ -13,6 +13,7 @@ import numpy as np
 from database_generation import flatfield as flatfield
 from pathlib import Path
 import pickle
+from database_generation.experimental_utils import plot_CCDimage
 
 #import sys
 #sys.path.append('/Users/lindamegner/MATS/MATS-retrieval/MATS-L1-processing/src/database_generation')
@@ -26,17 +27,29 @@ channels=['IR1','IR2','IR3','IR4','UV1','UV2']#,'NADIR' ]
 
 for channel in channels:
      #Note, only using HSM mode images for making flatfield now LM230925
-    flatfield_morphed=flatfield.make_flatfield(channel, calibration_file, plotresult=True, plotallplots=True)
+    flatfield_morphed, flatfield_wo_baffle_err, baffle_scalefield=flatfield.make_flatfield(channel, calibration_file, plotresult=True, plotallplots=True)
     Path("output").mkdir(parents=True, exist_ok=True)
     np.savetxt('output/flatfield_'+channel+'_HSM.csv', flatfield_morphed)
     np.save('output/flatfield_'+channel+'_HSM.npy', flatfield_morphed)
+    # The error measured as the standard deviation of three images divided by square root of 3
+    np.savetxt('output/flatfield_wo_baffle_err_scaled_'+channel+'_HSM.csv', flatfield_wo_baffle_err)
+    np.save('output/flatfield_wo_baffle_err_'+channel+'_HSM.npy', flatfield_wo_baffle_err)
+    # The baffle scalefield, where 1 means no effect, ie the flatfield is the 
+    # same as the one without baffle. 
+    np.savetxt('output/baffle_scalefield_'+channel+'_HSM.csv', baffle_scalefield)
+    np.save('output/baffle_scalefield_'+channel+'_HSM.npy', baffle_scalefield)
+
+
+    plot_CCDimage(flatfield_morphed, title='Flatfield '+channel)
+    plot_CCDimage(flatfield_wo_baffle_err, title='Flatfield wo error '+channel)
+    plot_CCDimage(baffle_scalefield, title='Baffe scalefield '+channel)
+
+
+
     #pickle the flatfields
     #pickle.dump(flatfield_morphed, open('output/flatfield_'+channel+'_HSM.pkl', 'wb'))
 
 
-
-#
-    
 
 
 # %%
